044d697bbd7ea02d6453d444533d69fb15e462b2
youngclown <ku9cu@naver.com>
2019-07-15 11:32:27 +0900

    단일 파일도 검색가능하게 변경

--- src/main/java/search/FileInSearch.java
+++ src/main/java/search/FileInSearch.java
@@ -18,60 +18,69 @@ public abstract class FileInSearch implements FileSearch {
         int cnt = 0; // 총 카운트 수
         File[] listOfFiles = FileUtil.fileList(PATH);
 
-        if(listOfFiles == null) {
-            System.out.println("file not found");
-            return -1;
+        if (listOfFiles == null) {
+            File file = new File(PATH);
+            filePlay(file, inKeyword, orKeyword, exceptKeyword);
+        } else {
+            for (File file : listOfFiles) {
+                filePlay(file, inKeyword, orKeyword, exceptKeyword);
+            }
         }
 
-        for (File file : listOfFiles) {
-            try (FileReader fileReader = new FileReader(file);
-                 BufferedReader bufReader = new BufferedReader(fileReader)){
+        return cnt;
+    }
 
-                String line;
-                while ((line = bufReader.readLine()) != null) {
-                    boolean patternOn = false;
 
-                    // 해당 배열이 or 조건으로 존재하면 무조건 true
-                    for (String keyword : orKeyword) {
-                        if (line.contains(keyword)) {
-                            patternOn = true;
-                            break;
-                        }
+
+    public void filePlay(File file,
+                         final String[] inKeyword,
+                         final String[] orKeyword,
+                         final String[] exceptKeyword) {
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufReader = new BufferedReader(fileReader)){
+
+            String line;
+            while ((line = bufReader.readLine()) != null) {
+                boolean patternOn = false;
+
+                // 해당 배열이 or 조건으로 존재하면 무조건 true
+                for (String keyword : orKeyword) {
+                    if (line.contains(keyword)) {
+                        patternOn = true;
+                        break;
                     }
+                }
 
-                    // 해당 배열이 전부 있어야 함.
-                    for (String keyword : inKeyword) {
+                // 해당 배열이 전부 있어야 함.
+                for (String keyword : inKeyword) {
+                    if (line.contains(keyword)) {
+                        patternOn = true;
+                    } else {
+                        patternOn = false;
+                        break;
+                    }
+                }
+
+                // 해당 배열이 하나라도 있으면 false
+                if (patternOn && exceptKeyword.length > 0) {
+                    for (String keyword : exceptKeyword) {
                         if (line.contains(keyword)) {
-                            patternOn = true;
-                        } else {
                             patternOn = false;
                             break;
                         }
                     }
+                }
 
-                    // 해당 배열이 하나라도 있으면 false
-                    if (patternOn && exceptKeyword.length > 0) {
-                        for (String keyword : exceptKeyword) {
-                            if (line.contains(keyword)) {
-                                patternOn = false;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (patternOn) {
-                        try {
-                            cnt++;
-                            checkPattern(line);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                if (patternOn) {
+                    try {
+                        checkPattern(line);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return cnt;
     }
 }
